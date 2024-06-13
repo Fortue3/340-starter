56c53a5e0709f7a2730f1e75c761d714b9f6f47f
Fortue3 <kwe22002@byui.edu>
2024-06-13 14:44:08 +0200

    correction

--- server.js
+++ server.js
@@ -6,11 +6,18 @@
  * Require Statements
  *************************/
 const express = require("express")
-const dotenv = require("dotenv").config()
 const expressLayouts = require("express-ejs-layouts")
+const dotenv = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
